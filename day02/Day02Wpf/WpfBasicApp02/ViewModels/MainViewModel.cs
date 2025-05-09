aadf34ab7c103f0f33e644c696ec353ae53f9ee9
HugoMGSung <personar95@naver.com>
2025-05-09 17:20:57 +0900

    2일차 커밋

--- day02/Day02Wpf/WpfBasicApp02/ViewModels/MainViewModel.cs
+++ day02/Day02Wpf/WpfBasicApp02/ViewModels/MainViewModel.cs
@@ -1,55 +1,53 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using MySql.Data.MySqlClient;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Windows;
+using WpfBasicApp02.Models;
 
-using WpfBasicApp02.Model;
-
-namespace WpfBasicApp02.ViewModel
+namespace WpfBasicApp02.ViewModels
 {
-    // MainViewModel에 속하는 속성의 값이 변경되면 이벤트가 발생.
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : Conductor<object>
     {
-        // 속성추가 get; set; 
-        // ObservableCollection -> 리스트의 변형(변화를 감지할 수 있도록 처리된 클래스)
-        public ObservableCollection<Book> Books { get; set; }
-
-        // List<KeyValuePair<string, string>> divisions 의 변형
         public ObservableCollection<KeyValuePair<string, string>> Divisions { get; set; }
 
-        // 선택된값에 대한 멤버변수, 멤버변수는 _를 붙이거나, 소문자로 변수명을 시작
+        public ObservableCollection<Book> Books { get; set; }
+
         private Book _selectedBook;
-        // 선택된값에 대한 속성
+
         public Book SelectedBook
         {
-            get => _selectedBook;  // 람다식 get { return _selectedBook; }와 동일
+            get => _selectedBook;
             set
             {
                 _selectedBook = value;
-                // 값이 변경된 것을 알아차리도록 해줘야 함!!!
-                OnPropertyChanged(nameof(SelectedBook)); // "SelectedBook"
+                NotifyOfPropertyChange(() => SelectedBook);
             }
         }
 
+        private readonly IDialogCoordinator _dialogCoordinator;
 
-        public MainViewModel() {
+        public MainViewModel(IDialogCoordinator dialogCoordinator) 
+        {            
+            _dialogCoordinator = dialogCoordinator;
             LoadControlFromDb();
             LoadGridFromDb();
         }
 
-        // DB에서 콤보박스에 넣을 데이터를 불러오기
+        //public MainViewModel()
+        //{
+        //    LoadControlFromDb();
+        //    LoadGridFromDb();
+        //}
+
         private void LoadControlFromDb()
         {
             // 1. 연결문자열(DB연결문자열은 필수)
             string connectionString = "Server=localhost;Database=bookrentalshop;Uid=root;Pwd=12345;Charset=utf8;";
             // 2. 사용쿼리
             string query = "SELECT division, names FROM divtbl";
-            
+
             ObservableCollection<KeyValuePair<string, string>> divisions = new ObservableCollection<KeyValuePair<string, string>>();
 
             // 3. DB연결, 명령, 리더
@@ -76,10 +74,9 @@ namespace WpfBasicApp02.ViewModel
             } // conn.Close() 자동발생
 
             Divisions = divisions;
-            OnPropertyChanged(nameof(Divisions));  // Divisions 속성값이 변경됨!
+            NotifyOfPropertyChange(() => Divisions); // Caliburn.Micro가 제공하는 메서드
         }
 
-        // DB에서 데이터 로드후 Books 속성에 집어넣기
         private void LoadGridFromDb()
         {
             // 1. 연결문자열(DB연결문자열은 필수)
@@ -124,16 +121,11 @@ namespace WpfBasicApp02.ViewModel
             } // conn.Close() 자동발생
 
             Books = books;
-            OnPropertyChanged(nameof(Books));
+            NotifyOfPropertyChange(() => Books);
         }
 
-        // 속성값이 변경되면 이벤트를 발생
-        public event PropertyChangedEventHandler? PropertyChanged;
-        
-        protected void OnPropertyChanged(string name)
+        public async void DoAction()
         {
-            // 기본적인 이벤트핸들러 파라미터와 동일(object sender, EventArgs e)
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
